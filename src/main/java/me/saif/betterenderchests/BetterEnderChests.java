4ba1fdb854fb81ca2de65c536360417db149d7d6
minion325 <saifmohammed966@gmail.com>
2022-05-19 23:52:29 -0400

    Initial commit

---
+++ src/main/java/me/saif/betterenderchests/BetterEnderChests.java
@@ -0,0 +1,17 @@
+package me.saif.betterenderchests;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BetterEnderChests extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
