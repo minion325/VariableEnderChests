d15c98b8f11779ae17340ca5f24faf2b03724d00
minion325 <saifmohammed966@gmail.com>
2025-04-27 21:30:55 -0400

    started work on a retrieval inventory for inaccesible items

--- src/main/java/me/saif/betterenderchests/enderchest/EnderChest.java
+++ src/main/java/me/saif/betterenderchests/enderchest/EnderChest.java
@@ -2,37 +2,39 @@ package me.saif.betterenderchests.enderchest;
 
 import me.saif.betterenderchests.hooks.ChestSortHook;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
-import java.util.function.BiConsumer;
 
 public class EnderChest implements InventoryHolder {
 
     public static final Map<Integer, String> INVENTORY_NAMES;
     public static final String PREFIX = "VECEnderChest";
+    public static final String RETRIEVAL_NAME = "VECRetrieval;<player>";
 
     static {
-        Map<Integer, String> temp = new HashMap<>();
-        temp.put(1, PREFIX + ";1;<player>");
-        temp.put(2, PREFIX + ";2;<player>");
-        temp.put(3, PREFIX + ";3;<player>");
-        temp.put(4, PREFIX + ";4;<player>");
-        temp.put(5, PREFIX + ";5;<player>");
-        temp.put(6, PREFIX + ";6;<player>");
+        Map<Integer, String> invNames = new HashMap<>();
+        invNames.put(1, PREFIX + ";1;<player>");
+        invNames.put(2, PREFIX + ";2;<player>");
+        invNames.put(3, PREFIX + ";3;<player>");
+        invNames.put(4, PREFIX + ";4;<player>");
+        invNames.put(5, PREFIX + ";5;<player>");
+        invNames.put(6, PREFIX + ";6;<player>");
 
-        INVENTORY_NAMES = Collections.unmodifiableMap(temp);
+        INVENTORY_NAMES = Collections.unmodifiableMap(invNames);
     }
 
     private final UUID UUID;
     private final String name;
     private ItemStack[] contents;
     private Inventory inventory;
+    private final EnderRetrievalInventory retrivalHolder = new EnderRetrievalInventory();
+    private Inventory retrievalInventory;
     private final Map<Integer, String> inventoryNames = new HashMap<>();
     private int lastNumRows = 6;
 
@@ -43,9 +45,15 @@ public class EnderChest implements InventoryHolder {
             this.inventoryNames.put(integer, s.replace("<player>", name));
         });
         this.contents = contents.length == 54 ? contents : Arrays.copyOf(contents, 54);
-        this.inventory = Bukkit.createInventory(this, lastNumRows * 6, this.inventoryNames.get(lastNumRows));
+        this.inventory = Bukkit.createInventory(this, lastNumRows * 9, this.inventoryNames.get(lastNumRows));
+
+        int retrievalSize = 54 - lastNumRows * 9;
+
+        this.retrievalInventory = Bukkit.createInventory(this.retrivalHolder, retrievalSize == 0 ? 9 : retrievalSize, RETRIEVAL_NAME);
+        this.retrivalHolder.setInventory(this.retrievalInventory);
 
         ChestSortHook.setSortable(this.inventory);
+        ChestSortHook.setUnsortable(this.retrievalInventory);
 
         populateInventory();
     }
@@ -96,8 +104,9 @@ public class EnderChest implements InventoryHolder {
     }
 
     public boolean hasViewers() {
-        List<HumanEntity> viewers = this.getInventory().getViewers();
-        return viewers.size() > 0;
+        List<HumanEntity> viewers = this.inventory.getViewers();
+        List<HumanEntity> retrievalViewers = this.retrievalInventory.getViewers();
+        return viewers.isEmpty() || retrievalViewers.isEmpty();
     }
 
     protected void openInventory(Player player) {
@@ -111,6 +120,13 @@ public class EnderChest implements InventoryHolder {
         for (int i = 0; i < inventory.getSize(); i++) {
             inventory.setItem(i, this.contents[i]);
         }
+
+        if (inventory.getSize() == 54)
+            return;
+
+        for (int i = 0; i < retrievalInventory.getSize(); i++) {
+            retrievalInventory.setItem(i, this.contents[i + inventory.getSize()]);
+        }
     }
 
     public ItemStack[] getContents() {
@@ -122,6 +138,13 @@ public class EnderChest implements InventoryHolder {
     private void updateContentsArray() {
         for (int i = 0; i < inventory.getSize(); i++) {
             this.contents[i] = inventory.getItem(i);
+        }
+
+        if (inventory.getSize() == 54)
+            return;
+
+        for (int i = inventory.getSize(); i < 54; i++) {
+            this.contents[i] = this.retrievalInventory.getItem(i - inventory.getSize());
         }
     }
 
@@ -139,5 +162,22 @@ public class EnderChest implements InventoryHolder {
 
     public EnderChestSnapshot snapshot() {
         return new EnderChestSnapshot(this);
+    }
+
+    public static class EnderRetrievalInventory implements InventoryHolder {
+
+        private Inventory inventory;
+
+        private EnderRetrievalInventory() {}
+
+        @NotNull
+        @Override
+        public Inventory getInventory() {
+            return inventory;
+        }
+
+        private void setInventory(Inventory inventory) {
+            this.inventory = inventory;
+        }
     }
 }
